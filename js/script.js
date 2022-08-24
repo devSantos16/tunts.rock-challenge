271f82caf15769b6573916b7d52aceef045ef5cc
devSantos16 <devsantos1999@gmail.com>
2022-08-24 11:24:52 -0300

    Mudando a Regra de negocio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 import fetch from 'node-fetch';
 
-
-
-
 class Country {
     constructor(name, capital, area, currencies) {
         this.name = name;
@@ -13,21 +10,54 @@ class Country {
 
 }
 
-async function setCountryAPI(listCountry) {
+async function Program(listCountry) {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
 
-    for (let i = 0; i < data.length; i++) {
-        listCountry.push(new Country(data[i].name.common, data[i].capital, data[i].area, data[i].currencies));
+
+    const verifyUndefined = (str) => {
+        if (str === undefined) {
+            return '-'
+        }
+        return str.toString();
     }
 
-    return listCountry;
+    const verifyCurrency = (currency) => {
+        // Perccorre no objeto de currencies e concatena todos os Currency
+        let currencies = '';
+
+        for (let value in currency) {
+            currencies += `${value},`
+        }
+
+        // Se currency não for vazio ele formata a string removendo a ultima virgula, caso não, apenas bota um traço
+        if (currencies) {
+            currencies = currencies.substring(0, currencies.length - 1);
+            return currencies;
+        }
+
+        return "-";
+
+    }
+
+
+    for (let i = 0; i < data.length; i++) {
+
+        const name = verifyUndefined(data[i].name.common);
+        const capital = verifyUndefined(data[i].capital);
+        const area = data[i].area;
+        const currency = await verifyCurrency(data[i].currencies);
+
+        const country = new Country(name, capital, area, currency);
+
+
+    }
+
 }
 
 async function run() {
     let listCountry = [];
-    await setCountryAPI(listCountry);
-    console.log(listCountry)
+    await Program(listCountry);
 }
 
 
